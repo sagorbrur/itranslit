6491b1e4a9f39f9eb98c6474022a7c22f7e362d4
sagorbrur <sagorhem3532@gmail.com>
2021-04-29 17:51:29 +0600

    itranslit initiated

---
+++ setup.py
@@ -0,0 +1,26 @@
+import codecs
+import setuptools
+
+
+setuptools.setup(
+    name="itranslit",
+    version="1.0.0",
+    author="Sagor Sarker",
+    author_email="sagorhem3532@gmail.com",
+    description="iTRANSLIT is a deep learning based transliteration package for indic language",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sagorbrur/bntranslit",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "wasabi"
+    ],
+)
